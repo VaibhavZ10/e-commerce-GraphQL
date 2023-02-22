4f6f6090318100387316aaef3eb0c07807880c60
vaibhav.sindal@gmail.com <vaibhav.sindal@gmail.com>
2023-02-22 17:04:18 +0530

    added mutations

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-
 // import { ApolloServer, gql } from "apollo-server-express";
-const {ApolloServer} = require('apollo-server');
-const { Category } = require('./resolvers/Category');
-const { Product } = require('./resolvers/Products');
-const { Query } = require('./resolvers/Query');
+const { ApolloServer } = require("apollo-server");
+const { Category } = require("./resolvers/Category");
+const { Product } = require("./resolvers/Products");
+const { Query } = require("./resolvers/Query");
+const { Mutation } = require("./resolvers/Mutation");
 const { typeDefs } = require("./schema");
-const {products,categories,reviews } = require("./db");
+const { db } = require("./db");
 
 //String, int, boolean, float -- Scalar Types || ID!
 //there are also object types
-
 
 const server = new ApolloServer({
   typeDefs,
   resolvers: {
     Query,
+    Mutation,
     Category,
     Product,
   },
   context: {
-    products,
-    categories,
-    reviews,
+    db,
   },
 });
 
 server.listen().then(({ url }) => {
-    console.log("Server up and runnning at " + url);
-})
- 
+  console.log("Server up and runnning at " + url);
+});
--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,39 +1,39 @@
-
 exports.Query = {
-  products: (parent, { filter }, { products, reviews }) => {
-    let filteredProducts = products;
+  products: (parent, { filter }, { db }) => {
+    let filteredProducts = db.products;
+    console.log(filteredProducts);
 
     if (filter) {
-      const {onSale, avgRating} = filter
+      const { onSale, avgRating } = filter;
       if (onSale) {
-        filteredProducts = filteredProducts.filter(product => {
+        filteredProducts = filteredProducts.filter((product) => {
           return product.onSale;
-        })
+        });
       }
       if ([1, 2, 3, 4, 5].includes(avgRating)) {
-        filteredProducts - filteredProducts.filter((product) => {
-          let sumRating = 0;
-          let numberOfReviews = 0;
-          reviews.forEach(review => {
-            if (review.productId === product.id) {
-              sumRating += review.rating;
-              numberOfReviews++;
-            }
+        filteredProducts -
+          filteredProducts.filter((product) => {
+            let sumRating = 0;
+            let numberOfReviews = 0;
+            db.reviews.forEach((review) => {
+              if (review.productId === product.id) {
+                sumRating += review.rating;
+                numberOfReviews++;
+              }
+            });
+            const avgProductRating = sumRating / numberOfReviews;
+
+            return avgProductRating >= avgRating;
           });
-          const avgProductRating = sumRating / numberOfReviews;
-
-          return avgProductRating >= avgRating;
-
-        });
       }
     }
     return filteredProducts;
   },
-  product: (parent, {id}, { products }) => {
-    return products.find((product) => product.id === id);
+  product: (parent, { id }, { db }) => {
+    return db.products.find((product) => product.id === id);
   },
-  categories: (parent, args, {categories}) => categories,
-  category: (parent, {id}, {categories}) => {
-    return categories.find((category) => category.id === id);
+  categories: (parent, args, { db }) => db.categories,
+  category: (parent, { id }, { db }) => {
+    return db.categories.find((category) => category.id === id);
   },
 };
